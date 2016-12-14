d758691591cf66c2376e1e6fb689c58a35b2c94c
Sagar Acharya <sagar@ytel.co.in>
2016-12-14 17:11:48 +0530

    wav file change

--- js/pitchdetect.js
+++ js/pitchdetect.js
@@ -43,7 +43,7 @@ window.onload = function() {
 	audioContext = new AudioContext();
 	MAX_SIZE = Math.max(4,Math.floor(audioContext.sampleRate/5000));	// corresponds to a 5kHz signal
 	var request = new XMLHttpRequest();
-	request.open("GET", "sounds/monkey-scream.wav", true);
+	request.open("GET", "sounds/silent.wav", true);
 	request.responseType = "arraybuffer";
 	request.onload = function() {
 	  audioContext.decodeAudioData( request.response, function(buffer) { 
